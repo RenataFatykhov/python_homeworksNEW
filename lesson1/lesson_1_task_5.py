afed32215550532f7ac8e2e90d51f7704908114b
RenataFatykhov <renata.musenova@gmail.com>
2024-07-05 21:57:41 +0300

    Changes to be committed: new file:   lesson1/lesson_1_task_2.py new file:   lesson1/lesson_1_task_3.py new file:   lesson1/lesson_1_task_4.py new file:   lesson1/lesson_1_task_5.py new file:   lesson1/lesson_1_task_6.py new file:   lesson1/lesson_1_task_7.py

---
+++ lesson1/lesson_1_task_5.py
@@ -0,0 +1,3 @@
+def print_greeting(a):
+    print(a)
+print_greeting('Hello, World!')
---
+++ lesson1/lesson_1_task_6.py
@@ -0,0 +1,31 @@
+def print_1(a):
+    print(a, end="")
+
+def print_2(b):
+    print(b, end="")
+
+def print_3(c):
+    print(c, end="")
+
+def print_4(d):
+    print(d, end="")
+
+def print_5(e):
+    print(e, end="")
+
+def print_6(f):
+    print(f, end="")
+
+def print_7(g):
+    print(g, end="")
+
+def print_8(k):
+    print(k, end="")
+
+def print_9(l):
+    print(l, end="")
+
+def print_0(m):
+    print(m, end="")
+
+print_8(8), print_8(8), print_0(0), print_0(0), print_5(5), print_5(5), print_5(5), print_3(3), print_5(5), print_3(3), print_5(5)
