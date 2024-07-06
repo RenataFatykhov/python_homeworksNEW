c97da4c5629779e86024109c7c0983b5e13dc07b
RenataFatykhov <renata.musenova@gmail.com>
2024-07-06 12:21:25 +0300

    Changes to be committed: modified:   lesson1/lesson_1_task_5.py modified:   lesson1/lesson_1_task_6.py

--- lesson1/lesson_1_task_6.py
+++ lesson1/lesson_1_task_6.py
@@ -1,31 +1,41 @@
-def print_1(a):
-    print(a, end="")
+def print_1():
+    print(1, end='')
 
-def print_2(b):
-    print(b, end="")
+def print_2():
+    print(2, end='')
 
-def print_3(c):
-    print(c, end="")
+def print_3():
+    print(3, end='')
 
-def print_4(d):
-    print(d, end="")
+def print_4():
+    print(4, end='')
 
-def print_5(e):
-    print(e, end="")
+def print_5():
+    print(5, end='')
 
-def print_6(f):
-    print(f, end="")
+def print_6():
+    print(6, end='')
 
-def print_7(g):
-    print(g, end="")
+def print_7():
+    print(7, end='')
 
-def print_8(k):
-    print(k, end="")
+def print_8():
+    print(8, end='')
 
-def print_9(l):
-    print(l, end="")
+def print_9():
+    print(9, end='')
 
-def print_0(m):
-    print(m, end="")
+def print_0():
+    print(0, end='')
 
-print_8(8), print_8(8), print_0(0), print_0(0), print_5(5), print_5(5), print_5(5), print_3(3), print_5(5), print_3(3), print_5(5)
+print_8()
+print_8()
+print_0()
+print_0()
+print_5()
+print_5()
+print_5()
+print_3()
+print_5()
+print_3()
+print_5()
